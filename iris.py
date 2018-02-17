1e14f1995db51030231868319939c9ddf153a598
Matt Runyon <mtrunyon91@gmail.com>
2018-02-17 17:22:56 -0500

    Initial commit: iris.py, readme.md

---
+++ iris.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Title: iris.py
+Date: February 17th, 2018
+Author: Matt Runyon
+Description: This script explores the iris flower data set. This is a test
+             case typical for many statistical classification techniques in
+             machine learning.
+"""
+
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
---
+++ readme.md
@@ -0,0 +1,1 @@
+Nothing cool, just me trying to teach myself stuff :)
