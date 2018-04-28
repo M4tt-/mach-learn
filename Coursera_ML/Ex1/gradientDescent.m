0a370ac08f524adc9957e811c8d984dbe2f36f2b
M4tt- <mtrunyon91@gmail.com>
2018-04-28 14:45:15 -0400

    scripted single variate gradient descent

--- Coursera_ML/Ex1/gradientDescent.m
+++ Coursera_ML/Ex1/gradientDescent.m
@@ -12,14 +12,15 @@ for iter = 1:num_iters
     % Compute gradient
     hyp = X*theta;                % hypothesis
     err = hyp - y;                % error
-    fprintf("Printing size of err: %d %d\n", size(err))
-    fprintf("Printing size of X: %d %d\n", size(X))
+    
+    %fprintf("Printing size of err: %d %d\n", size(err))
+    %fprintf("Printing size of X: %d %d\n", size(X))
     gradient0 = (m^(-1)).*sum(err);        % Gradient for constant term
-    fprintf("Printing size of gradient0: %d %d\n", size(gradient0))
+    %fprintf("Printing size of gradient0: %d %d\n", size(gradient0))
     gradient1 = (m^(-1)).*sum(err'*X(:,2));     % Gradient for first order term
-    fprintf("Printing size of gradient1: %d %d\n", size(gradient1))
+    %fprintf("Printing size of gradient1: %d %d\n", size(gradient1))
     gradient = [gradient0; gradient1];
-    fprintf("Gradient: %f\n", gradient);
+    %fprintf("Gradient: %f\n", gradient);
     
     % Update theta vectorally
     theta = theta - alpha.*gradient;
