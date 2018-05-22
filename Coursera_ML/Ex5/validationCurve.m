884050bf4ebc147f77e7e7f8b04b4e68f0a6fd7c
M4tt- <mtrunyon91@gmail.com>
2018-05-22 18:36:22 -0400

    Completed Exercise/Assignent #5

---
+++ Coursera_ML/Ex5/validationCurve.m
@@ -0,0 +1,62 @@
+function [lambda_vec, error_train, error_val] = ...
+    validationCurve(X, y, Xval, yval)
+%VALIDATIONCURVE Generate the train and validation errors needed to
+%plot a validation curve that we can use to select lambda
+%   [lambda_vec, error_train, error_val] = ...
+%       VALIDATIONCURVE(X, y, Xval, yval) returns the train
+%       and validation errors (in error_train, error_val)
+%       for different values of lambda. You are given the training set (X,
+%       y) and validation set (Xval, yval).
+%
+
+% Selected values of lambda (you should not change this)
+lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+
+% You need to return these variables correctly.
+error_train = zeros(length(lambda_vec), 1);
+error_val = zeros(length(lambda_vec), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return training errors in 
+%               error_train and the validation errors in error_val. The 
+%               vector lambda_vec contains the different lambda parameters 
+%               to use for each calculation of the errors, i.e, 
+%               error_train(i), and error_val(i) should give 
+%               you the errors obtained after training with 
+%               lambda = lambda_vec(i)
+%
+% Note: You can loop over lambda_vec with the following:
+%
+%       for i = 1:length(lambda_vec)
+%           lambda = lambda_vec(i);
+%           % Compute train / val errors when training linear 
+%           % regression with regularization parameter lambda
+%           % You should store the result in error_train(i)
+%           % and error_val(i)
+%           ....
+%           
+%       end
+%
+%
+
+for i=1:length(lambda_vec)
+
+    % Pick a value of lambda
+    lambda = lambda_vec(i);
+    lambda0 = 0;
+
+    % Get theta parameter(s)
+    theta = trainLinearReg(X, y, lambda);
+
+    % Compute train error / validation error
+    [terror, ~] = linearRegCostFunction(X, y, theta, lambda0);
+    [verror, ~] = linearRegCostFunction(Xval, yval, theta, lambda0);
+
+    % Append to return data structures
+    error_train(i) = terror;
+    error_val(i) = verror;
+end
+
+% =========================================================================
+
+end
